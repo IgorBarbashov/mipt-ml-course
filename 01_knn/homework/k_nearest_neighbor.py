0964ffc0838c7b5f3dcfae5bd6589149cf197bf9
Igor Barbashov <bis200705@gmail.com>
2026-02-15 18:40:42 +0300

    [01-knn] Сделал compute_distances_one_loop

--- 01_knn/homework/k_nearest_neighbor.py
+++ 01_knn/homework/k_nearest_neighbor.py
@@ -126,7 +126,7 @@ class KNearestNeighbor:
 
                 # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
                 diff = X[i] - self.X_train[j]
-                dists[i, j] = np.sqrt(np.sum(diff**2))
+                dists[i, j] = np.sqrt(np.sum(diff ** 2))
                 # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return dists
 
@@ -163,8 +163,8 @@ class KNearestNeighbor:
             ########################################################################
 
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-            pass
-
+            diff = X[i] - self.X_train
+            dists[i, :] = np.sqrt(np.sum(diff ** 2, axis=1))
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return dists
 
